2135fd642ace859e4a30caae3d9a627e24f48589
irfan <irfa@gmail.com>
2026-09-21 16:25:58 +0700

    perubahan favicon

--- resources/views/layouts/commonMaster.blade.php
+++ resources/views/layouts/commonMaster.blade.php
@@ -1,44 +1,55 @@
 <!DOCTYPE html>
 
-<html lang="{{ session()->get('locale') ?? app()->getLocale() }}" class="{{ $configData['style'] }}-style {{ $navbarFixed ?? '' }} {{ $menuFixed ?? '' }} {{ $menuCollapsed ?? '' }} {{ $footerFixed ?? '' }} {{ $customizerHidden ?? '' }}" dir="{{ $configData['textDirection'] }}" data-theme="{{ $configData['theme'] }}" data-assets-path="{{ asset('/assets') . '/' }}" data-base-url="{{url('/')}}" data-framework="laravel" data-template="{{ $configData['layout'] . '-menu-' . $configData['theme'] . '-' . $configData['style'] }}">
+<html lang="{{ session()->get('locale') ?? app()->getLocale() }}"
+    class="{{ $configData['style'] }}-style {{ $navbarFixed ?? '' }} {{ $menuFixed ?? '' }} {{ $menuCollapsed ?? '' }} {{ $footerFixed ?? '' }} {{ $customizerHidden ?? '' }}"
+    dir="{{ $configData['textDirection'] }}" data-theme="{{ $configData['theme'] }}"
+    data-assets-path="{{ asset('/assets') . '/' }}" data-base-url="{{ url('/') }}" data-framework="laravel"
+    data-template="{{ $configData['layout'] . '-menu-' . $configData['theme'] . '-' . $configData['style'] }}">
 
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta charset="utf-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-  <title>@yield('title') |
-    {{ config('variables.templateName') ? config('variables.templateName') : 'TemplateName' }} -
-    {{ config('variables.templateSuffix') ? config('variables.templateSuffix') : 'TemplateSuffix' }}
-  </title>
-  <meta name="description" content="{{ config('variables.templateDescription') ? config('variables.templateDescription') : '' }}" />
-  <meta name="keywords" content="{{ config('variables.templateKeyword') ? config('variables.templateKeyword') : '' }}">
-  <!-- laravel CRUD token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <!-- Canonical SEO -->
-  <link rel="canonical" href="{{ config('variables.productPage') ? config('variables.productPage') : '' }}">
-  <!-- Favicon -->
-  <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon/favicon.ico') }}" />
+    <title>@yield('title') |
+        {{ config('variables.templateName') ? config('variables.templateName') : 'TemplateName' }} -
+        {{ config('variables.templateSuffix') ? config('variables.templateSuffix') : 'TemplateSuffix' }}
+    </title>
+    <meta name="description"
+        content="{{ config('variables.templateDescription') ? config('variables.templateDescription') : '' }}" />
+    <meta name="keywords"
+        content="{{ config('variables.templateKeyword') ? config('variables.templateKeyword') : '' }}">
+    <!-- laravel CRUD token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Canonical SEO -->
+    <link rel="canonical" href="{{ config('variables.productPage') ? config('variables.productPage') : '' }}">
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon_io/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon_io/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon_io/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('favicon_io/site.webmanifest') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon_io/favicon.ico') }}" />
 
-  
 
-  <!-- Include Styles -->
-  @include('layouts/sections/styles')
 
-  <!-- Include Scripts for customizer, helper, analytics, config -->
-  @include('layouts/sections/scriptsIncludes')
+    <!-- Include Styles -->
+    @include('layouts/sections/styles')
+
+    <!-- Include Scripts for customizer, helper, analytics, config -->
+    @include('layouts/sections/scriptsIncludes')
 </head>
 
 <body>
-  
 
-  <!-- Layout Content -->
-  @yield('layoutContent')
-  <!--/ Layout Content -->
 
-  
+    <!-- Layout Content -->
+    @yield('layoutContent')
+    <!--/ Layout Content -->
 
-  <!-- Include Scripts -->
-  @include('layouts/sections/scripts')
+
+
+    <!-- Include Scripts -->
+    @include('layouts/sections/scripts')
 
 </body>
 
