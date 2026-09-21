bd707c83623918320c8e61faf7effaebc47c42ca
irfan <irfa@gmail.com>
2026-09-21 16:12:40 +0700

    perubahan provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
    */
   public function boot(): void
   {
-    if (config('app.env') !== 'local') {
+    if (config('app.env') !== 'local' || request()->server('HTTP_X_FORWARDED_PROTO') === 'https') {
       URL::forceScheme('https');
     }
   }
